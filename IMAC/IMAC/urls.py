d9a648d9d0a1e7c7e475880d0d58a3dab130c583
Rafiarian <5026211177@mhs.its.ac.id>
2023-01-19 00:54:03 +0700

    404 PAGE DONE

--- IMAC/IMAC/urls.py
+++ IMAC/IMAC/urls.py
@@ -23,6 +23,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('mainweb.urls')),
+    path('error/', include('mainweb.urls')),
     path('', RedirectView.as_view(url='home/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler404 = "mainweb.views.handle_not_found"
--- IMAC/tailwind.config.js
+++ IMAC/tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: [
     './mainweb/templates/**/*.html',
-    './node_modules/flowbite/**/*.js'
+    './node_modules/flowbite/**/*.js',
+    './components/**/*.{html,py}',
+    './pages/**/*.{html,py}',
+
   ],
   theme: {
     extend: {},
