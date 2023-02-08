ec81a7dd7a047a46d0341a4198a1eb9cb302b9d6
achvents <achmadfadjarm@gmail.com>
2023-02-08 22:06:58 +0700

    modified:   IMAC/mainweb/admin.py 	deleted:    IMAC/IMAC/etc/secret_key.txt 	deleted:    IMAC/IMAC/productionsettings.py 	modified:   IMAC/IMAC/settings.py 	modified:   IMAC/IMAC/urls.py 	modified:   IMAC/db.sqlite3 	modified:   IMAC/mainweb/migrations/0001_initial.py 	deleted:    IMAC/mainweb/migrations/0002_remove_article_contributor_alter_member_date_joined_and_more.py 	deleted:    IMAC/mainweb/migrations/0003_alter_member_is_active.py 	deleted:    IMAC/mainweb/migrations/0004_alter_member_date_joined.py 	deleted:    IMAC/mainweb/migrations/0005_alter_member_is_active.py 	modified:   IMAC/mainweb/migrations/__pycache__/0001_initial.cpython-310.pyc 	modified:   IMAC/mainweb/migrations/__pycache__/__init__.cpython-310.pyc 	modified:   IMAC/mainweb/models.py 	modified:   IMAC/mainweb/templates/base_generic.html 	modified:   IMAC/mainweb/templates/error.html 	modified:   IMAC/mainweb/templates/home.html 	deleted:    IMAC/mainweb/templates/login.html 	modified:   IMAC/mainweb/urls.py 	modified:   IMAC/mainweb/views.py 	modified:   IMAC/static/src/output - Copy.css 	modified:   IMAC/static/src/output.css 	modified:   IMAC/tailwind.config.js 	IMAC/mainweb/forms.py 	IMAC/mainweb/migrations/0002_auto_20230202_2210.py 	IMAC/mainweb/migrations/0003_auto_20230202_2210.py 	IMAC/mainweb/migrations/0004_alter_nama_manajer_manajer_divisi.py 	IMAC/mainweb/templates/admitted.html 	IMAC/mainweb/templates/nama_manajer.html 	IMAC/mainweb/templates/registration/ 	IMAC/mainweb/templates/rejected.html 	IMAC/mainweb/templates/uploadlolos.html 	IMAC/mainweb/utils.py 	IMAC/requirements.txt 	IMAC/static/bego_imac_generic.png

--- IMAC/mainweb/admin.py
+++ IMAC/mainweb/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import member, IMACevents, article
+from .models import IMACevents, article, User, Nama_Manajer, Divisi
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-admin.site.register(member, UserAdmin)
+#Kalo bikin model baru jangan lupa masukin sini yaa (Register dibawah)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'name', 'last_login','nrp','IMACid','usergroup')}),
+        ('Permissions', {'fields': (
+            'is_active', 
+            'is_staff', 
+            'is_superuser',
+            'groups', 
+            'user_permissions'
+        )}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('IMACid','email','name', 'password1', 'password2','groups', 'nrp','usergroup')
+            }
+        ),
+    )
+
+    list_display = ('email', 'name', 'is_staff', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+
+admin.site.register(User, UserAdmin)
+
 admin.site.register(IMACevents)
 admin.site.register(article)
-
+admin.site.register(Divisi)
+admin.site.register(Nama_Manajer)
