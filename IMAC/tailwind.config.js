1a3690b277aafc737a26f7b2a0ea5df210d440e6
Daud Dhiya <daud.dhiya31@gmail.com>
2023-03-02 01:30:52 +0700

    feat: add tailwind custom color

--- IMAC/tailwind.config.js
+++ IMAC/tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './mainweb/templates/**/*.html',
-    './node_modules/flowbite/**/*.js'
-  ],
+  content: ["./mainweb/templates/**/*.html", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBrown: "#AF4B21",
+        mainCream: "#D8D5CA",
+        mainDarkGreen: "#0A262A",
+        mainGray: "#0A262A40",
+      },
+    },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/line-clamp")],
+};
